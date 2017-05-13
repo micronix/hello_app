7df83d6d8513ddaf2a228c6b3f40ca7f6d9c847d
Code the Dream <dan@unitingnc.org>
2017-05-13 15:52:05 +0000

    removed style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-      render inline: "<h1 style='font-size: 2em;'>hello world</h1>"
+      render inline: "<h1>hello world</h1>"
   end
 end
