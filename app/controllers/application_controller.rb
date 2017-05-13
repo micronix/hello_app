fc6d0b46d5a19608e636be8b325f529de006a00a
Code the Dream <dan@unitingnc.org>
2017-05-13 15:49:00 +0000

    adds hello message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def hello
+      render inline: "<h1 style='font-size: 2em;'>hello world</h1>"
+  end
 end
